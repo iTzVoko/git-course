e53b4fce94a144364c3e936ca9c7ed640b7b2c32
iTzVoko <vokoads@gmail.com>
2023-06-02 16:58:10 +0300

    إضافة التحقق من مُعطيات سطر الأوامر (الرقم واللأُس)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
 #include <cmath>
 
-int main() {
+void print_usage(std::string program_path) {
+	std::cerr << "Too few arguments\n\n"
+		<< "Usage:\n\t" << program_path << " number [power]\n";
+}
+
+int main(int argc, char **argv) {
+	if (argc < 2) {
+		print_usage(argv[0]);
+		std::exit(EXIT_FAILURE);
+	}
+
 	int number = 10;
 	int power = 4;
 	
